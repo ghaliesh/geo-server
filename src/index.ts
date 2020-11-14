a51ff264dec995a768d42f056ab792bb4d30a39d
Ghazwan Aliesh <ghazwan@pragmateam.dev>
2020-11-14 20:07:30 +0200

    feat: initiate server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
 import { config as initiateEnv } from "dotenv";
+import express from "express";
+
+import { getEnv } from "./utils";
+import { IEnvArgs } from "./utils/utils.types";
 
 /** initiate the env variables in .env files */
 initiateEnv();
 
-console.log(process.env["PORT"]);
+const port = getEnv(IEnvArgs.PORT);
+
+const server = express();
+
+const onServerStarted = () =>
+  console.info(`Server is now running on http://localhost:${port}`);
+
+server.listen(port, onServerStarted);
