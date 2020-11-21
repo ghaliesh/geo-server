6af2c29b172023082860c80cfc30544018aaba1d
Ghazwan Aliesh <ghazwan@pragmateam.dev>
2020-11-21 21:37:26 +0200

    config: add env example

--- src/utils/dbconfig.ts
+++ src/utils/dbconfig.ts
@@ -2,10 +2,11 @@ import { getEnv } from "./env";
 import { IEnvArgs } from "./utils.types";
 
 export const getDataBaseURI = () => {
+  const databaseURI: string = getEnv(IEnvArgs.DATABASE_URL);
   const username: string = getEnv(IEnvArgs.DATABASE_USER_NAME);
   const password: string = encodeURIComponent(
     getEnv(IEnvArgs.DATABASE_USER_PASSWORD)
   );
 
-  return `mongodb://${username}:${password}@ds041484.mlab.com:41484/gps`;
+  return `mongodb://${username}:${password}@${databaseURI}`;
 };
