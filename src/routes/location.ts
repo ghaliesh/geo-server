a7dd22be24235ec7c76ff9accbccb45b4f08ea51
Ghazwan Aliesh <ghazwan@pragmateam.dev>
2020-12-04 20:04:36 +0200

    chor: remove unused packages

--- src/routes/location.ts
+++ src/routes/location.ts
@@ -8,9 +8,6 @@ locationRoute.get(
   "/ip",
   async (req, res): Promise<void> => {
     try {
-      console.log({ ip: 1 });
-      const ip: IPResult = await getIp();
-      console.log({ ip });
       res.status(200).send({ ip: req.ip });
     } catch (err) {
       res.status(500).send({ error: "Request failed" });
