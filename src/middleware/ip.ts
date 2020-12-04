485c492533fa4c4ee08b170e97d69bd4fa5b1410
Ghazwan Aliesh <ghazwan@pragmateam.dev>
2020-12-04 20:45:35 +0200

    bug: ip middleware wasn't calling next :/

--- src/middleware/ip.ts
+++ src/middleware/ip.ts
@@ -1,10 +1,15 @@
 import { AppRequest, AppResponse } from "@geo/types";
 
-export const attachUserIp = (req: AppRequest, res: AppResponse) => {
+export const attachUserIp = (
+  req: AppRequest,
+  res: AppResponse,
+  next: Function
+) => {
   try {
     let ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
     ip = ip?.toString()?.replace("::ffff:", "");
     req.userIp = ip;
+    next();
   } catch (error) {
     res.status(500).send({ error });
   }
